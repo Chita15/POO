c628bbdd6dc4b9bdd6738f9fdc83e4cebdfa867b
Chita15 <vnsslov21@gmail.com>
2020-02-19 19:28:39 -0800

    Update Program.cs

--- Peliculas/Program.cs
+++ Peliculas/Program.cs
@@ -25,7 +25,7 @@ namespace Peliculas
             peli2.titulo="Green Book";
             peli2.año=2018;
             //peli2.pais="Canadá";
-            //peli2.director="Peter Farrelly";
+            peli2.director="Peter Farrelly";
 
             Console.WriteLine("Título:{0} Año:{1}", peli2.titulo, peli2.año, peli2.pais, peli2.director);
         }
