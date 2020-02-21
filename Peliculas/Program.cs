122632da7ab5c678306c700f18e282f928ce18eb
Chita15 <vnsslov21@gmail.com>
2020-02-20 19:13:38 -0800

    Update Program.cs

--- Peliculas/Program.cs
+++ Peliculas/Program.cs
@@ -9,42 +9,42 @@ namespace Peliculas
         private string pais;
         private string director;
 
-        public string getTitulo()
+        public string GetTitulo()
         {
             return titulo;
         }
 
-        public void setTitulo(string t)
+        public void SetTitulo(string t)
         {
             titulo = t;
         }
 
-        public int getAño()
+        public int GetAño()
         {
             return año;
         }
 
-        public void setAño(int a)
+        public void SetAño(int a)
         {
             año = a;
         }
 
-        public string getPais()
+        public string GetPais()
         {
             return pais;
         }
 
-        public void setPais(string p)
+        public void SetPais(string p)
         {
             pais = p;
         }
 
-        public string getDirector()
+        public string GetDirector()
         {
             return director;
         }
 
-        public void setDirector(string d)
+        public void SetDirector(string d)
         {
             director = d;
         }
@@ -53,21 +53,21 @@ namespace Peliculas
     {
         static void Main(string[] args)
         {
-            Pelicula peli1= new Pelicula();
-            peli1.setTitulo("Parasites");
-            peli1.setAño(2019);
-            peli1.setPais("Corea del Sur");
-            peli1.setDirector("Boong Joon-ho");
+            Pelicula peli1 = new Pelicula();
+            peli1.SetTitulo("Parasites");
+            peli1.SetAño(2019);
+            peli1.SetPais("Corea del Sur");
+            peli1.SetDirector("Boong Joon-ho");
 
-            Console.WriteLine("Título:{0} Año:{1} País:{2} Director:{3}", peli1.getTitulo(), peli1.getAño(), peli1.getPais(), peli1.getDirector());
+            Console.WriteLine("Título:{0} Año:{1} País:{2} Director:{3}", peli1.GetTitulo(), peli1.GetAño(), peli1.GetPais(), peli1.GetDirector());
 
-            Pelicula peli2= new Pelicula();
-            peli2.setTitulo("Green Book");
-            peli2.setAño(2018);
-            peli2.setPais("Canadá");
-            peli2.setDirector("Peter Farrelly");
+            Pelicula peli2 = new Pelicula();
+            peli2.SetTitulo("Green Book");
+            peli2.SetAño(2018);
+            peli2.SetPais("Canadá");
+            peli2.SetDirector("Peter Farrelly");
 
-            Console.WriteLine("Título:{0} Año:{1} País:{2} Director:{3}", peli2.getTitulo(), peli2.getAño(), peli2.getPais(), peli2.getDirector());
+            Console.WriteLine("Título:{0} Año:{1} País:{2} Director:{3}", peli2.GetTitulo(), peli2.GetAño(), peli2.GetPais(), peli2.GetDirector());
         }
     }
 }
