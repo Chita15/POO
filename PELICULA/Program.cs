57aa09d49e8b2f73816a7774ef35f1983f273fd9
Chita15 <vnsslov21@gmail.com>
2020-02-21 20:34:24 -0800

    Update Program.cs

--- PELICULA/Program.cs
+++ PELICULA/Program.cs
@@ -4,32 +4,75 @@ namespace PELICULA
 {
      class Pelicula
     {
-        public string titulo;
-        public int año;
-        public string pais;
-        public string director;
+        private string titulo;
+        private int año;
+        private string pais;
+        private string director;
+
+                public string GetTitulo()
+        {
+            return titulo;
+        }
+
+        public void SetTitulo(string t)
+        {
+            titulo = t;
+        }
+
+        public int GetAño()
+        {
+            return año;
+        }
+
+        public void SetAño(int a)
+        {
+            año = a;
+        }
+
+        public string GetPais()
+        {
+            return pais;
+        }
+
+        public void SetPais(string p)
+        {
+            pais = p;
+        }
+
+        public string GetDirector()
+        {
+            return director;
+        }
+
+        public void SetDirector(string d)
+        {
+            director = d;
+        }
     }
+
     class Program
     {
         static void Main(string[] args)
         {
-            //Creación del objeto peli1. Datos de la primer pelicula ganadora a oscar a asignar
-            Pelicula peli1= new Pelicula();
-            peli1.titulo="Parasites";
-            peli1.año=2019;
-            //peli1.pais="Corea del Sur";---> Se comentan ya que el ejercicio solicita solo el Titulo y año
-            //peli1.director="Boong Joon-ho";--->Misma razón, ya mencionada antes
+            //Creación del objeto peli1 y uso de Set y Get para asignar valores. Primer pelicula ganadora del oscar
+            Pelicula peli1 = new Pelicula();
+            peli1.SetTitulo("Parasites");
+            peli1.SetAño(2019);
+            peli1.SetPais("Corea del Sur");
+            peli1.SetDirector("Boong Joon-ho");
 
-            Console.WriteLine("Título:{0} Año:{1}", peli1.titulo, peli1.año, peli1.pais, peli1.director);
+            //Impresión de los datos
+            Console.WriteLine("Título:{0} Año:{1} País:{2} Director:{3}", peli1.GetTitulo(), peli1.GetAño(), peli1.GetPais(), peli1.GetDirector());
 
-             //Creación del objeto peli2. Datos de la segunda pelicula ganadora a oscar a asignar
-            Pelicula peli2= new Pelicula();
-            peli2.titulo="Green Book";
-            peli2.año=2018;
-            //peli2.pais="Canadá"; ---> Se comentan ya que el ejercicio solicita solo el Titulo y año
-            //peli2.director="Peter Farrelly"; --->Misma razón, ya mencionada antes
+            //Creación del peli2 con uso de Set y Get. Segunda pelicula ganadora del oscar
+            Pelicula peli2 = new Pelicula();
+            peli2.SetTitulo("Green Book");
+            peli2.SetAño(2018);
+            peli2.SetPais("Canadá");
+            peli2.SetDirector("Peter Farrelly");
 
-            Console.WriteLine("Título:{0} Año:{1}", peli2.titulo, peli2.año, peli2.pais, peli2.director);
+            //Impresión de los datos de la segunda pelicula seleccionada
+            Console.WriteLine("Título:{0} Año:{1} País:{2} Director:{3}", peli2.GetTitulo(), peli2.GetAño(), peli2.GetPais(), peli2.GetDirector());
         }
     }
 }
