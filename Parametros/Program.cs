5aef1aa7d191adfefda93667a4322e59b8c29a2f
Chita15 <vnsslov21@gmail.com>
2020-03-09 16:17:11 -0700

    Create Program.cs

---
+++ Parametros/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Parametros
+{
+    class Hora
+    {
+        	public void tiempo(int t) 
+            { 
+			    t = t*60;
+            }
+
+            public void tiempo_ref(ref int t)
+            {
+                t = t*30;
+            }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Hora reloj = new Hora();
+
+             reloj.tiempo(1);
+
+             int num = 2;
+
+             reloj.tiempo(num);
+
+             Console.WriteLine("Segundos transcurridos: {0}", num);
+        }
+    }
+}
