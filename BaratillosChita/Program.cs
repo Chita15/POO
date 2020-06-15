933d3c06cc673c3d5d46f639dab6d425ede20abb
Chita15 <vnsslov21@gmail.com>
2020-06-15 01:39:35 -0700

    Update photothumb.db

---
+++ BaratillosChita/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BaratillosChita
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
